5ea365d0e65083e8dab4db83f0702391f18697a5
David Charbonnier <david.charbonnier@cloudimperiumgames.com>
2025-10-07 08:54:59 -0400

    fix: make sure we use java17 on android for all subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,7 +11,20 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Apply Java 17 configuration to all subprojects to eliminate warnings
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
